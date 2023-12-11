5a0a337013bd232ae54928f6f7458c6b53e06602
Jon Griggs <bigmac_beats1773@outlook.com>
2023-12-10 23:03:09 -0600

    made algo more efficient

--- script.js
+++ script.js
@@ -1,22 +1,19 @@
 let binary = "1011011101111"
 
-const convertBinaryToDecimal = (num) => {
-    let doubles = []
-    let decimal = 0
-    let start = 1
+
+// console.log(convertBinaryToDecimal(binary))
+
+const convertBinaryToDecimal = num => {
+    let decimal = 0;
+    let start = 1;
     for (let i = num.length - 1; i >= 0; i--) {
-        console.log(i, start, doubles)
-        if (num[i] == "1") {
-            doubles.push(start)
-            start *= 2
-        } else {
-            start *= 2
+        if (num[i] === "1") {
+            decimal += start;
         }
+        start *= 2;
     }
-    for (let j = 0; j < doubles.length; j++) {
-        decimal += doubles[j]
-    }
-    return decimal
+    return decimal;
 }
+
 
 console.log(convertBinaryToDecimal(binary))
