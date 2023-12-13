03c10b725c74f5cc09fd0d702f824148803308f1
Jon Griggs <bigmac_beats1773@outlook.com>
2023-12-13 13:43:02 -0600

    created function to calculate eduction percentage

--- script.js
+++ script.js
@@ -1,39 +1,52 @@
-let binary = "10000001101000111000"
-let decimal = "531000"
+let binary = "10000001101000111000";
+let decimal = "531000";
 
 // console.log(convertBinaryToDecimal(binary))
 
-const convertBinaryToDecimal = num => {
+const convertBinaryToDecimal = (num) => {
     let decimal = 0;
     let start = 1;
     for (let i = num.length - 1; i >= 0; i--) {
         if (num[i] === "1") {
-            decimal += start;
+        decimal += start;
         }
         start *= 2;
     }
     return decimal;
-}
+};
 
+// console.log(convertBinaryToDecimal(binary));
 
-console.log(convertBinaryToDecimal(binary))
-
-const convertDecimalToBinary = num => {
-    let binaryNum = ""
-    let numToReverse = ""
-    while(num >= 1) {
-        if(num % 2 != 0) {
-            numToReverse += "1"
+const convertDecimalToBinary = (num) => {
+    let binaryNum = "";
+    let numToReverse = "";
+    while (num >= 1) {
+        if (num % 2 != 0) {
+        numToReverse += "1";
+        } else {
+        numToReverse += "0";
         }
-        else {
-            numToReverse += "0"
-        }
-        num = parseInt(num / 2)
+        num = parseInt(num / 2);
     }
-    for(let i = numToReverse.length - 1; i >= 0; i--) {
-        binaryNum += numToReverse[i]
+    for (let i = numToReverse.length - 1; i >= 0; i--) {
+        binaryNum += numToReverse[i];
     }
-    return binaryNum
-}
+    return binaryNum;
+};
 
 // console.log(convertDecimalToBinary(decimal))
+
+const findReductionPercentage = (orgVal, newVal) => {
+    let absVal = orgVal - newVal;
+    let percReduction = absVal / orgVal;
+    percReduction *= 100
+    if(percReduction > 0) {
+        return "You have a reduction of " + percReduction.toFixed(2) + "%"
+    }
+    else {
+        percReduction *= -1
+        return "You have an increase of " + percReduction.toFixed(2) + "%"
+    }
+};
+
+console.log(findReductionPercentage(10, 24))
